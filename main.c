5837c66b35396c7c42dfc388a332d614ae68399e
Tomás Umbelino <tomasumbelino03@gmail.com>
2021-12-29 22:22:48 +0000

    ok

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#include "funcoesGenericas.h"
+#include "gestaoMenu.h"
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    printf("Mekie");
+    return 0;
+}
