f6c94e1e9399c679f328049687044f226e9b6e1a
Tomás Umbelino <tomasumbelino03@gmail.com>
2021-12-30 16:47:40 +0000

    Atualização menu #3

--- gestaoMenu.c
+++ gestaoMenu.c
@@ -8,17 +8,15 @@ int menuPrincipal(int nrMembros, int nrTestesAgendados, int nrTestesRealizados, 
     int opcao;
 
     printf("\n**********************************************************");
-    printf("\n*\t\t GESTAO COVID-19\t\t\t *");
+    printf("\n*\t\t\tGESTAO COVID-19\t\t\t *");
     printf("\n**********************************************************");
-    printf("\nMembros registados: %4d\tTestes realizados: %4d", nrMembros, nrTestesRealizados);
-    printf("\nTestes agendados: %6d\tMembros vacinados: %4d", nrTestesAgendados, nrMembrosVacinados);
+    printf("\n   Membros registados: %4d\tTestes realizados: %4d", nrMembros, nrTestesRealizados);
+    printf("\n   Testes agendados: %6d\tMembros vacinados: %4d", nrTestesAgendados, nrMembrosVacinados);
     printf("\n**********************************************************");
-    printf("\n[1] - Estados de vacinação;");
-    printf("\n[2] - Estados de confinamento;");
-    printf("\n[3] - Agendar teste de diagnóstico;");
-    printf("\n[4] - Listar dados de testes de diagnóstico;");
-    printf("\n[5] - Alterar a data de agendamento de teste de diagnóstico;");
-    printf("\n[6] - Informações de testagem;");
+    printf("\n[1] - Gerir membros;");
+    printf("\n[2] - Estados de vacinação;");
+    printf("\n[3] - Estados de confinamento;");
+    printf("\n[4] - Gerir testes de diagnóstico;");
     printf("\n[0] - Sair.");
     printf("\n\nEscolha uma das opções: ");
 
